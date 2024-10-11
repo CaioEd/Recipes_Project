74ec2d941ee36a200868a3dafa46df3b3409f1b2
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2024-10-11 14:01:24 -0300

    novas alterações

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 # HTTP REQUEST
 def home(request):
     # HTTP RESPONSE
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Caio'
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
